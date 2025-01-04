a7fac28a1edcc70aa5e435d4d7ddf20fca59aacb
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2025-01-04 12:47:05 +0300

    Начал работу над проектом, внес некоторые правки в структуру и зависимости.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,21 +39,27 @@ android {
     }
 }
 
+// Библиотеки для работы с Android
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+// Библиотеки для Jetpack Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+// Зависимости для тестирования
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+// Инструменты для отладки
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
